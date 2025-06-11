f7174183770f173449a185a556bf12cb55446177
Navya Sai Sadu <161189318+Navya-1817@users.noreply.github.com>
2025-06-11 08:40:53 +0530

    Update DBConnection.java

--- ASAP/src/main/java/util/DBConnection.java
+++ ASAP/src/main/java/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/complaint_system";
     private static final String USER = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "h0p3"; // Replace with your MySQL password
+    private static final String PASSWORD = "password"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         try {
